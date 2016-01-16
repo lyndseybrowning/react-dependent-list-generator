69e88b768f7b7f1067131dde67b268e3144264f8
Lyndsey Browning <lbrowning86@gmail.com>
2016-01-16 11:54:29 +0000

    Added new sort function that sorts object properties

--- js/helpers.js
+++ js/helpers.js
@@ -9,6 +9,21 @@ let helpers = {
       return 1;
     }
     return 0;
+  },
+
+  // Sort by object property (prop)
+  sortAlphaObj(prop, a, b) {
+    let sortA = a[prop].toLowerCase(),
+        sortB = b[prop].toLowerCase();
+
+    if(sortA < sortB) {
+      return -1;
+    }
+
+    if(sortA > sortB) {
+      return 1;
+    }
+    return 0;
   }
 
 };
